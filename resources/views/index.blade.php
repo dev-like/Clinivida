26171387bd5f1995440bb687e063fe234e018e6d
dev@l1k3 <dev@likedsg.com>
2022-05-03 16:24:48 -0300

    ADD: menu expansivo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,7 +95,51 @@
             </div>
         </header>
 
-        <header class="header-middle header-middle_white-transparent">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="navbar-logo">
+                <a href="{{url("/")}}">
+                    <img src="../site/images/logo.png" />
+                </a>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Alterna navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <!-- <a class="navbar-brand" href="#">Marca escondida</a> -->
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <!-- <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                </li> -->
+                <li class="nav-item">
+                    <a class="nav-link" href="#sobre">Quem Somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#exames">Exames</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#equipe">Equipe</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#convenio">Convênios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#localizacao">Localização</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contato">Contato</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Desativado</a>
+                </li> -->
+                </ul>
+                <!-- <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                </form> -->
+            </div>
+        </nav>
+
+        <!-- <header class="header-middle header-middle_white-transparent">
             <div class="container">
 
                 <div class="navbar-logo">
@@ -104,7 +148,11 @@
                     </a>
                 </div>
 
+                
+
                 <div class="navbar">
+
+                    
 
                     <ul class="navbar__menu">
                         <li class="navbar__menu-item">
@@ -143,7 +191,7 @@
                 </div>
 
             </div>
-        </header>
+        </header> -->
 
         <header class="header-bottom">
 
--- resources/views/partials/_css.blade.php
+++ resources/views/partials/_css.blade.php
@@ -3,3 +3,6 @@
 
       <!-- template style -->
       <link rel="stylesheet" href="../site/css/main.css">
+
+      <!-- estilo do navbar toggle -->
+      <link rel="stylesheet" href="../site/css/navbar_toggle.css">
